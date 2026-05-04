519d6d91174f16ab856dc87e0386ac843b9d2bdd
Ga-rima8 <pandeygarima2005@gmail.com>
2026-05-04 14:46:51 +0545

    Added DBconfig.java for database connection

---
+++ Garima/src/main/java/com/java_web_app/utils/DBconfig.java
@@ -0,0 +1,20 @@
+package com.java_web_app.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconfig {
+    private static final String URL = "jdbc:mysql://localhost:3306/hangaura_db";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+    
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL Driver not found!", e);
+        }
+    }
+}
