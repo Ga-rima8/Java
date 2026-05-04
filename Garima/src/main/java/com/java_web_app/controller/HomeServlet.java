8243f0a3b55423881c24015cb63bf7c5ed2d1e0b
Ga-rima8 <pandeygarima2005@gmail.com>
2026-05-04 08:58:41 +0545

    Fix: add Garima as regular folder

---
+++ Garima/src/main/java/com/java_web_app/controller/HomeServlet.java
@@ -0,0 +1,31 @@
+package com.java_web_app.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(asyncSupported = true, urlPatterns = { "/home" })
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public HomeServlet() {
+        super();
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/index.jsp").forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
+
+
